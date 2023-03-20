61c85ae0a9ce3592e4e10b4008d15d4895306086
“Vasiliy <vasenka_kuznetsov_1997@mail.ru>
2023-03-20 21:44:01 +0300

    Initial commit

---
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -0,0 +1,35 @@
+package ru.netology;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CardOrderTest {
+    private WebDriver driver;
+
+
+    @BeforeAll
+    static void setUpAll(){
+        System.setProperty("webdriver.chrome.driver", "./Driver/Win/chromedriver.exe");
+    }
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void tearDown(){
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+     void shouldTestSomething(){
+        throw new UnsupportedOperationException();
+    }
+
+
+}
